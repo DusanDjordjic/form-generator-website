9566dde75a2480758e4613968c98069cd50035bc
Dusan Djordjic <77781108+DusanDjordjic@users.noreply.github.com>
2021-11-22 14:13:05 +0100

    Generator page design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutpageComponent } from './about/aboutpage/aboutpage.component';
 import { ErrorpageComponent } from './core/errorpage/errorpage.component';
+import { GeneratorPageComponent } from './generator/generator-page/generator-page.component';
 import { HomepageComponent } from './home/homepage/homepage.component';
 import { PricingpageComponent } from './pricing/pricingpage/pricingpage.component';
 
 const routes: Routes = [
   { path: '', component: HomepageComponent, pathMatch: 'full' },
-  { path: 'generator', component: PricingpageComponent },
+  { path: 'generator', component: GeneratorPageComponent },
   { path: 'about', component: AboutpageComponent },
   { path: 'pricing', component: PricingpageComponent },
   { path: '**', component: ErrorpageComponent },
